651a65ab7fba23a703a816c78014a70004511c3b
David Pedrero <pedrero888@gmail.com>
2022-11-23 22:21:17 -0500

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
+// Components
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      {/* SideDrawer */ }
+      {/* Backdrop */ }
+      <main>
+        <Routes>
+          <Route/>
+          <Route exact path="/" element={<HomeScreen/>} />
+          <Route exact path='/product/:id' element={<ProductScreen/>}/>
+          <Route exact path='/cart' element={<CartScreen/>}/>
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProductScreen.css'
+
+const CartScreen = () => {
+    return (
+        <div className='cartscreen'>Cart Screen</div>
+    )
+}
+
+export default CartScreen
