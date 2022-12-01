f91d3ff6dd845fbccf95e19a3bf1abcb638c973e
David Pedrero <pedrero888@gmail.com>
2022-12-01 15:52:46 -0500

    sideDrawer added/congifgured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
@@ -11,13 +12,17 @@ import CartScreen from './screens/CartScreen';
 
 // Components
 import Navbar from './components/Navbar';
+import Backdrop from './components/Backdrop';
+import SideDrawer from './components/SideDrawer';
 
 function App() {
+
+  const [sideToggle, setSideToggle] = useState(false);
   return (
     <Router>
-      <Navbar/>
-      {/* SideDrawer */ }
-      {/* Backdrop */ }
+      <Navbar click={() => setSideToggle(true)}/>
+      <SideDrawer show={sideToggle}/>
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)}/>
       <main>
         <Routes>
           <Route/>
