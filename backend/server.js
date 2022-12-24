2e79aaf5c3141cca05e155cc3a6f262eb3ca3b28
David Pedrero <pedrero888@gmail.com>
2022-12-24 01:38:01 -0500

    report web vitals comment out2

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRoutes);
 
-if(process.env.NODE_ENV === "production") {
+if(process.env.NODE_ENV !== "development") {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
     app.get('*', (req, res) => {
@@ -24,6 +24,6 @@ if(process.env.NODE_ENV === "production") {
     })
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
