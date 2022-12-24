351c8f1829130b4f80c93d9af9722ceaec3028dc
David Pedrero <pedrero888@gmail.com>
2022-12-24 11:09:04 -0500

    hail mary attempt

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api/products', productRoutes);
 
 if(process.env.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
-
+    
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
     });
@@ -23,6 +21,8 @@ if(process.env.NODE_ENV == "production") {
         res.send("Api is running...");
     })
 }
+
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
