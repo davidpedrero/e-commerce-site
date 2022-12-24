926d58e3c7657ec78be0f7bd7d9808f1018bc027
David Pedrero <pedrero888@gmail.com>
2022-12-24 01:00:07 -0500

    report web vitals comment out

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -19,4 +19,4 @@ root.render(
   , document.getElementById('root')
 );
 
-reportWebVitals();
+// reportWebVitals();
